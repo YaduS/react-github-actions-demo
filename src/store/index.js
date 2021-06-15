5f02ebf987af47f376d58e2940ce635eb74afa18
Yadu S <yadu.s@ccs-technologies.com>
2021-06-15 17:32:16 +0530

    completely using redux/toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -13,55 +13,13 @@ const counterSlice = createSlice({
       state.counter--;
     },
     increase(state, action) {
-      state.counter += action.value;
+      state.counter += action.payload;
     },
     toggle(state) {
       state.showCounter = !state.showCounter;
     },
   },
 });
-
-// const stateReducer = (state = initialState, action) => {
-//   if (action.type === 'increment') {
-//     return {
-//       counter: state.counter + 1,
-//       showCounter: state.showCounter,
-//     };
-//   }
-//   if (action.type === 'increase') {
-//     return {
-//       counter: state.counter + action.value,
-//       showCounter: state.showCounter,
-//     };
-//   }
-//   if (action.type === 'decrement') {
-//     return {
-//       counter: state.counter - 1,
-//       showCounter: state.showCounter,
-//     };
-//   }
-
-//   if (action.type === 'toggle') {
-//     return {
-//       showCounter: !state.showCounter,
-//       counter: state.counter,
-//     };
-//   }
-
-//   return state;
-// };
-
-// const store = createStore(stateReducer);
-
-// store.subscribe((event) => {
-//   console.log('in subscribe, event: ', event);
-//   const state = store.getState();
-//   console.log('state: ', state);
-// });
-
-// store.dispatch({ type: 'increment' });
-// store.dispatch({ type: 'decrement' });
-
 const store = configureStore({
   reducer: counterSlice.reducer,
 });
@@ -72,5 +30,7 @@ const store = configureStore({
 //     counter: counterSlice.reducer,
 //   },
 // });
+
+export const counterActions = counterSlice.actions;
 
 export default store;
